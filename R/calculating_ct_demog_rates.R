08ad422aaf324cd6109c1d8f1512e9fb5a7dc2d2
Tesla <tesla.dubois@gmail.com>
2022-01-12 11:08:15 -0500

    Squashed commit of the following:
    commit 8e7f880236df1da0263b620c013bfcd0f6204451
    Author: Jake <rjake@sas.upenn.edu>
    Date:   Mon Jan 10 19:09:34 2022 -0500
    
        Attempt
    
    commit d157c1c8efde5587f9fa114779a5e4d669ba2d86
    Author: Jake <rjake@sas.upenn.edu>
    Date:   Sat Jan 8 10:53:31 2022 -0500
    
        Create calculating_neighborhood_vars.Rproj
    
    commit b03514c481e2b10b1bf4a9ce45ae38f65287a120
    Author: Jake <rjake@sas.upenn.edu>
    Date:   Sat Jan 8 10:52:34 2022 -0500
    
        Create README.md
    
    commit 047e8859c47f566affa38786965090784150862c
    Author: Jake <rjake@sas.upenn.edu>
    Date:   Sat Jan 8 10:52:30 2022 -0500
    
        Create .gitignore
    
    commit c3712c5759b242ba87c836e3d1cea083fce26339
    Author: Jake <rjake@sas.upenn.edu>
    Date:   Sat Jan 8 10:51:15 2022 -0500
    
        Add R script
    
    commit 2cda8240b728be87f45ccc30f41811914e1f22be
    Merge: 390d05d 014ea45
    Author: Jake <rjake@sas.upenn.edu>
    Date:   Sat Jan 8 10:49:29 2022 -0500
    
        Merge branch 'main' into jake_review
    
    commit 390d05d620c6aaf83171a1005dd3af0f62a80c45
    Author: Tesla <95730807+tesla-dubois@users.noreply.github.com>
    Date:   Thu Jan 6 08:28:53 2022 -0500
    
        Delete README.md
    
    commit 92d5e28915733d23849878d26451a32ed2344a6f
    Author: Tesla <95730807+tesla-dubois@users.noreply.github.com>
    Date:   Wed Jan 5 10:14:24 2022 -0500
    
        Delete .Rhistory

---
+++ R/calculating_ct_demog_rates.R
@@ -0,0 +1,71 @@
+library(tidycensus)
+library(dplyr)
+library(tidyr)
+
+# GET VARIABLES
+vars <- load_variables(2019, "acs5", cache = TRUE)
+# View(vars)
+
+# VARIABLE NAMES
+var_names <- c("below_pov", "pop_pov", "NHBlack", "NHAsian", "pop_race", "HispanicAn", "housing_all_units", "housing_occ_units", "renterocc_hh", "hh_income", "Birth_foreign", "pop_born", "Educ_ltHS", "pop_ed", "pop_labor", "employed", "trans_tran", "pop_trans", paste0("samhous_pop_grp_", 1:2), paste0("samhous_grp", 1:2))
+
+var_codes <- c("B17020_002", "B17020_001", "B03002_004", "B03002_006", "B03002_001", "B03002_012", "B25002_001", "B25002_002", "B25003_003", "B19013_001", "B05002_013", "B05002_001", "B16010_002", "B16010_001", "B23025_003", "B23025_004", "B08006_008", "B08006_001", paste0("B07001_0", 1:2), paste0("B07001_0", 17:18))
+
+codebook <-
+  tibble(
+    name = var_codes,
+    short_desc = var_names
+  ) |>
+  inner_join(vars) |>
+  print()
+
+
+# PULLING 5-YEAR ACS ESTIMATES IN ENDING IN YEARS DEFINED BELOW.
+acs_data <- NULL
+
+year <- c(2018, 2019)
+states <- c("PA", "NJ")
+items <- set_names(codebook$name, codebook$short_desc)
+
+for (i in year) {
+  res <- get_acs(
+    geography = "state", survey = "acs5", variables = items,
+    state = states, year = i, key = census_key
+  )
+
+  temp <-
+    res |>
+    select(-moe) |>
+    mutate(year = i) |>
+    pivot_wider(names_from = variable, values_from = estimate)
+
+  acs_data <-
+    bind_rows(acs_data, temp)
+}
+
+
+acs_data |>
+  transmute(
+    GEOID,
+    NAME,
+    year,
+    prc_pov = below_pov / pop_pov,
+    across(
+      .cols = c(NHBlack, NHAsian, HispanicAn, Birth_foreign),
+      .fns = ~(.x / pop_race), # pop_race is the same as pop_total
+      .names = "prc_{.col}"
+    ),
+    prc_educ_ltHS = Educ_ltHS / pop_ed,
+    prc_employed = employed / pop_labor,
+    prc_trans_tran = trans_tran / pop_trans,
+    prc_renterocc_hh = renterocc_hh / housing_occ_units,
+    prc_same_house =                        #       total - ages 1-4
+      (samhous_grp1 - samhous_grp2) / ( samhous_pop_grp_1 - samhous_pop_grp_2),
+    hh_income
+   ) |>
+  mutate(
+    across(
+      .cols = starts_with("prc"),
+      .fns = ~round(.x * 100, 2))
+  ) |>
+  arrange(GEOID, year)
